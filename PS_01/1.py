9e0a6ee8a2ceb0ce21a7365c2dd4e60bf51254a0
Caio <caiosgon3@gmail.com>
2019-04-01 00:32:55 -0300

    Refactoring

---
+++ PS_01/1.py
@@ -0,0 +1,62 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+from window import Window
+from cg_util import plot_channels_hist,esc_pressed
+
+red_color = (0,0,255)
+white_color = (255,255,255)
+window_half_len = 20
+default_img, img = None,None
+
+def exercise_1():
+    cv2.imshow("Exercise 1.1", img)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+
+def exercise_2():
+    colors = ('b','g','r')
+    max_value = 256
+    plt.title("Exercise 1.2") 
+    plot_channels_hist(img, colors, max_value)
+    plt.show()
+
+def exercise_3():
+    cv2.namedWindow('Exercise 1.3')
+    cv2.setMouseCallback('Exercise 1.3',draw_window)  
+    cv2.imshow("Exercise 1.3", img)  
+    while(1):
+        key = cv2.waitKey(20) 
+        if esc_pressed(key):
+            break
+    cv2.destroyAllWindows()    
+
+def draw_window(event,x,y,flags,param):    
+    if event == cv2.EVENT_MOUSEMOVE:        
+        clean_image()
+        window = Window(img,x,y,window_half_len)
+        if window.it_fits():
+            window.draw_retangle(red_color)
+            window.put_informations(white_color)
+            window.draw_new_frame()
+        cv2.imshow("Exercise 1.3", img)
+
+def clean_image():
+    global img
+    img = default_img.copy()
+
+def init(image_path):
+    global default_img
+    default_img = cv2.imread(image_path)
+    clean_image()
+
+def main():
+    init("images/guardians1.png")
+    exercise_1()
+    exercise_2()
+    exercise_3()
+
+if __name__ == "__main__":
+    main()
+
+    
---
+++ PS_01/frequency_image.py
@@ -0,0 +1,51 @@
+import cv2
+import numpy as np
+from cg_util import calculate_image_measures
+
+class Frequency_Image:
+
+    fourier = None
+    fourier_shift = None
+    fourier_inverse = None
+    img_spectrum = None
+    img_back = None
+    magnitude = None
+    phase = None
+
+    def __init__(self, img):
+        self.fourier_transform(img)
+        self.init_magnitude_phase()
+        self.calculate_spectrum()
+        self.inverse_fourier_transform()
+
+    def init_magnitude_phase(self):
+        plane = cv2.split(self.fourier_shift)
+        self.magnitude, self.phase = cv2.cartToPolar(plane[0], plane[1])
+
+    def update_magnitude_phase(self, magnitude=None, phase=None):
+        if magnitude is not None:
+            self.magnitude = magnitude
+        if phase is not None:
+            self.phase = phase
+        self.calculate_spectrum()
+        self.inverse_fourier_transform()
+
+    def calculate_spectrum(self):
+        self.img_spectrum = 20*np.log(self.magnitude)
+
+    def get_magnitude_phase(self):
+        return self.magnitude, self.phase
+
+    def fourier_transform(self, img):
+        self.fourier = cv2.dft(np.float32(img), flags=cv2.DFT_COMPLEX_OUTPUT)
+        self.fourier_shift = np.fft.fftshift(self.fourier)
+
+    def inverse_fourier_transform(self):
+        plane = cv2.polarToCart(self.magnitude, self.phase)
+        fourier = cv2.merge(plane)
+        fourier_inv_shift = np.fft.ifftshift(fourier)
+        img_back = cv2.idft(fourier_inv_shift)    
+        new_plane = cv2.split(img_back)
+        self.img_back = cv2.magnitude(new_plane[0],new_plane[1])        
+
+    
